67fdda7399b53789e5c5271c44e6c5d5ec4925eb
devoschy <geisser@dublin-it.ch>
2017-02-03 01:41:55 +0100

    ready for volume in /var/PassWebData

--- Addons/docker/30-install.sh
+++ Addons/docker/30-install.sh
@@ -23,7 +23,6 @@ unzip master.zip && rm -f master.zip
 mv PassWebTool-master PassWebTool
 cd /opt/PassWebTool
 rm -rf Addons
-
 mkdir var/log
 
 echo ">>>>>>>>> $0: removing wget and tar <<<<<<<<<"
@@ -31,3 +30,30 @@ yum remove -y wget unzip
 echo ">>>>>>>>> $0: generating htpasswd <<<<<<<<<"
 htpasswd -b -c var/lib/pwt.htpasswd pwt password 
 
+echo ">>>>>>>>> $0: moving to volume <<<<<<<<<"
+cd /opt/PassWebTool
+if [ ! -e /var/PassWebData ]; then
+	mkdir -v /var/PassWebData
+fi
+
+if [ ! -e /var/PassWebData/lib ]; then
+	mv -v var/lib /var/PassWebData
+	rm -rf var/
+fi
+
+if [ ! -e /var/PassWebData/log/ ]; then
+	mkdir -v /var/PassWebData/log/
+	mv -v /var/log/httpd /var/PassWebData/log/ 
+	ln -v -s /var/PassWebData/log/httpd /var/log/httpd
+fi
+
+if [ ! -e var/ ]; then
+	ln -v -s /var/PassWebData/ ./var
+fi
+
+echo ">>>>>>>>> $0: chowning <<<<<<<<<"
+
+chown -v root:apache      /opt/PassWebTool/etc/*
+chown -v -R root:root     /opt/PassWebTool/bin/
+chown -v -R apache:root   /opt/PassWebTool/var/*
+chown -v root:root        /opt/PassWebTool/var/lib/pwt.htpasswd
--- Addons/docker/start.sh
+++ Addons/docker/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo ">>> this is $0 $@"
+
 echo ">>> including hostname into default vhost index.html"
 echo "<sub>(this is `hostname --fqdn` started `date`)</sub><br>" >> /var/www/html/index.html
 echo ">>> dumping config"
+httpd -v
 httpd -S
-echo ">>> starting httpd"
-
-httpd -DFOREGROUND
+echo ">>> exec httpd"
+exec httpd -DFOREGROUND
 echo ">>> httpd exited ($?)"
